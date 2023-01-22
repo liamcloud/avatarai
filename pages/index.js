0b55f987ed6a7279290fbecaa34b823a734ba7af
Rick Sun <liamcloud@protonmail.com>
2023-01-22 17:08:02 +0100

    sunday

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,16 @@ import "@fontsource/work-sans"
 import clouds from '../public/clouds.png'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
+  const [input, setInput] = useState('')
+  const onChange = (event) => {
+    setInput(event.target.value)
+  }
+  return(
     <>
       <Head>
         <title>Create Next App</title>
@@ -19,19 +24,26 @@ export default function Home() {
       <main className={styles.main}>
         <div>
           <h1 className='header'>AI Avatar Generator</h1>
-          <p className='paragraph'>Turn yourself into a greek philosopher</p>
+          <p className='paragraph'>Turn me into anyone you want! Make sure you refer to me as "<span>imliamcloud</span>" in the prompt</p>
         </div>
-        <div>
-          <h2 className='header'></h2>
+        <div className='promptContainer'>
+          <input placeholder="Portrait of imliamcloud by Michelangelo..." className="prompt-box" value={input} onChange={onChange} />
+          <div className="prompt-buttons">
+    <a className="generate-button">
+      <div className="generate">
+        <p>Generate</p>
+      </div>
+    </a>
+  </div>
         </div>
         <Image
       loader=""
       className='image'
       src={clouds}
       alt="Picture of the author"
-      height={700}
+      height={800}
         />
       </main>
     </>
-  )
+    )
 }
